abb877dbe233387f14ab818816734dcdd08a7ba8
Champ_R <mugambiwarwema@users.noreply.github.com>
2021-02-07 21:13:38 +0300

    added camera module

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CaptureService} from '../services/capture.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public captureService : CaptureService) {}
+
+  addPhotoToGallery(){
+    this.captureService.addNewToGallery();
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CaptureService} from '../services/capture.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  constructor(public captureService : CaptureService) {}
+  async ngOnInit() {
+    await this.captureService.loadSaved();
+  }
 }
